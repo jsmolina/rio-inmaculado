bd9a1f631ce14de6c062c4f849fc3b2dcc6fb37e
jsmolina <jordi.sesmeromolina@telefonica.com>
2024-03-21 11:23:36 +0100

    add tiled

--- static/convert.sh
+++ static/convert.sh
@@ -15,3 +15,4 @@ convert miguel_sprites.png -crop 40x40+400+0 -colors 256 -type truecolor miguel1
 #   convert miguel_sprites.png -crop 40x40+440+0 -colors 256 -type truecolor miguel11.pcx
 convert background.png -colors 256 -type truecolor bg.pcx
 convert background3.png -colors 256 -type truecolor bg3.pcx
+convert background4.png -colors 256 -type truecolor bg4.pcx
---
+++ static/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="8" tileheight="8" tilecount="364" columns="26">
+ <image source="new_tiles.png" width="209" height="116"/>
+</tileset>
